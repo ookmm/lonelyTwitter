c9d67d9710a9c2baffec70355ccd71f708f3f6ba
ookmm <gaylordmbuyi@gmail.com>
2016-10-18 19:11:49 -0600

    Finished lab7

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -30,8 +30,34 @@ public class ElasticsearchTweetController {
 
             ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
 
+            /*
             // assume that search_parameters[0] is the only search term we are interested in using
             Search search = new Search.Builder(search_parameters[0])
+                    .addIndex("testing")
+                    .addType("tweet")
+                    .build();
+            */
+
+            String query = "{\n"
+                    + "    \"query\": {\n"
+                    + "        \"filtered\" : {\n"
+                    + "            \"query\" : {\n"
+                    + "                \"query_string\" : {\n"
+                    + "                    \"message\" : \"\"\n"
+                    + "                }\n"
+                    + "            }"
+                    + "        }\n"
+                    + "    }\n"
+                    + "}";
+
+            // 10000 first tweets
+            //String search_string = "{\"from\":0, \"size\": 1000}";
+
+            // Search everything
+            String search_string = "{\"from\": 0, \"size\": 10000, \"query\": {\"match\": {\"message\": \"" +
+                    search_parameters[0] + "\"}}}";
+
+            Search search = new Search.Builder(search_string)
                     .addIndex("testing")
                     .addType("tweet")
                     .build();
